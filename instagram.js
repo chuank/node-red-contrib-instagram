06b2ebfce1d0e27948c346a236b56064256f3da3
Chuan Khoo <chuank@users.noreply.github.com>
2019-12-09 19:26:10 +1100

    Update instagram.js

--- instagram.js
+++ instagram.js
@@ -73,45 +73,51 @@ module.exports = function(RED) {
 
 		node.ig.use({ access_token: node.instagramConfig.credentials.access_token});
 
+
+		console.log("##############");
+		console.log(node.instagramConfig.credentials.access_token);
+		console.log("##############");
+
+
 		// Now grab initial state but only grab the ones we're concerned with
 
 		if (node.inputType === "photo") {
-			node.ig.user_media_recent("self", { count : 1, min_id : null, max_id : null}, function(err, medias, pagination, remaining, limit) {
-				if (err) {
-					node.warn(RED._("instagram.warn.userphoto-fetch-fail", {err: err}));
-				}
-
-				if(medias.length > 0) { // if the user has uploaded something to Instagram already
-					node.latestSelfContentID = medias[0].id;
-				}
-
-				node.on("input", function(msg) {
-					msg = {};
-					handleInputNodeInput(node, msg);
-				});
-				node.interval = setInterval(function() { // self trigger
-					node.emit("input", {});
-				}, repeat);
-			});
-		} else if (node.inputType === "like") {
-			node.ig.user_self_liked({ count : 1, max_like_id : null}, function(err, medias, pagination, remaining, limit) {
-				if (err) {
-					node.warn(RED._("instagram.warn.likedphoto-fetch-fail", {err: err}));
-				}
-
-				if(medias.length > 0) { // if the user has liked something to Instagram already
-					node.latestLikedID = medias[0].id;
-				}
-
-				node.on("input", function(msg) {
-					msg = {};
-					handleInputNodeInput(node, msg);
-				});
-
-				node.interval = setInterval(function() { // self trigger
-					node.emit("input", {});
-				}, repeat);
-			});
+		// 	node.ig.user_media_recent("self", { count : 1, min_id : null, max_id : null}, function(err, medias, pagination, remaining, limit) {
+		// 		if (err) {
+		// 			node.warn(RED._("instagram.warn.userphoto-fetch-fail", {err: err}));
+		// 		}
+		//
+		// 		if(medias.length > 0) { // if the user has uploaded something to Instagram already
+		// 			node.latestSelfContentID = medias[0].id;
+		// 		}
+		//
+		// 		node.on("input", function(msg) {
+		// 			msg = {};
+		// 			handleInputNodeInput(node, msg);
+		// 		});
+		// 		node.interval = setInterval(function() { // self trigger
+		// 			node.emit("input", {});
+		// 		}, repeat);
+		// 	});
+		// } else if (node.inputType === "like") {
+		// 	node.ig.user_self_liked({ count : 1, max_like_id : null}, function(err, medias, pagination, remaining, limit) {
+		// 		if (err) {
+		// 			node.warn(RED._("instagram.warn.likedphoto-fetch-fail", {err: err}));
+		// 		}
+		//
+		// 		if(medias.length > 0) { // if the user has liked something to Instagram already
+		// 			node.latestLikedID = medias[0].id;
+		// 		}
+		//
+		// 		node.on("input", function(msg) {
+		// 			msg = {};
+		// 			handleInputNodeInput(node, msg);
+		// 		});
+		//
+		// 		node.interval = setInterval(function() { // self trigger
+		// 			node.emit("input", {});
+		// 		}, repeat);
+		// 	});
 		}
 	}
 
